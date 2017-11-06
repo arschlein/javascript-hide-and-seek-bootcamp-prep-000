c9423d23299f6a390175b2fd403f327d19f4d49b
Monika <arschlein@seznam.cz>
2017-11-06 07:32:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function increaseRankBy(n) {
     let children = rankedLists[0].children
 
     for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n +12
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n -12
     }
   }
 }
