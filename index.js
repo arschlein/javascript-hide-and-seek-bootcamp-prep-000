42fc0d79d23750d33cac795bb0a76a0d49db273a
Monika <arschlein@seznam.cz>
2017-11-06 07:20:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n){
     let children = rankedLists[0].children
 
   for(let j = 0, k = children.length; j < k; j++){
-    children[j] = parseInt(children[j].innerHTML) + n
+    children[j].innerHTML = parseInt(children[j].innerHTML) + n
   }
 }
 }
