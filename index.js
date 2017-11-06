285d73a683783955cf95e294913111b73e399c37
Monika <arschlein@seznam.cz>
2017-11-06 07:22:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector("#nested .target");
 }
-function increaseRankBy(n){
-  const rankedLists = document.querySelectorAll(".ranked-list")
 
-  for(let i = 0, l = rankedLists.length; i < l; i++){
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
     let children = rankedLists[0].children
 
-  for(let j = 0, k = children.length; j < k; j++){
-    children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
   }
-}
 }
