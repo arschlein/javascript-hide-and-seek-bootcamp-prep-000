507ba85d6ef2c075cd6427424d4ab48354641084
Monika <arschlein@seznam.cz>
2017-11-06 07:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function getFirstSelector(selector){
-  return document.querySelectorAll(selector)
+  return document.querySelector(selector)
 }
 function nestedTarget(){
   return document.querySelector("#nested .target");
 }
 function increaseRankBy(n){
-  const rankedLists = document.querySelector(".ranked-list");
+  const rankedLists = document.querySelectorAll(".ranked-list");
 
   for(let i = 0, l = rankedLists.length; i < l; i++){
     let children = rankedLists[0].children
